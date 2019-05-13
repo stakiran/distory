f0692b70d341b1009b81b9df26c32861708a2ee2
stakiran <stakiran@users.noreply.github.com>
2019-05-13 21:13:00 +0900

    add.

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # distory
+Diary of your browser History.
+
+## Requirement
+- Windows 7+
+- Python 3.6+
+- Firefox Quantum
+
+## Preparation
+- (1) Prepare `sqlite3.exe` from [SQLite Download Page](https://www.sqlite.org/download.html)
+  - Ex. sqlite-tools-win32-x86-3280000.zip
+- (2) Get your Firefox profile name
+  - See `%appdata%\Mozilla\Firefox\Profiles`
+  - Ex1. `tvsczp3y.default`
+  - Ex2. `iotcz7up.stakiran`
+- (3) Get history data with CSV
+  - 
+
+## How to use
+Firstly, output .csv file.
+
+Secondly, output .md file from .csv file.
+
+For example, if you want to get between 19/05/13 and 19/05/14, like this:
+
+```
+$ python distory.py -p iotcz7up.stakiran -d 190513
+
+$ python distory.py -p iotcz7up.stakiran -d 190513 --md
+```
+
+And, files like this:
+
+190513.csv
+
+```
+...
+"Sign in to GitHub · GitHub",https://github.com/login?return_to=%2Fstakiran,1557748660862000
+"stakiran (stakiran)",https://github.com/stakiran,1557748668694000
+"Create a New Repository",https://github.com/new,1557748673761000
+```
+
+190513.md
+
+```
+# 2019/05/13(月) 77 counts
+...
+- 2019/05/13(月) 20:57:40 ["Sign in to GitHub · GitHub"](https://github.com/login?return_to=%2Fstakiran)
+- 2019/05/13(月) 20:57:48 ["stakiran (stakiran)"](https://github.com/stakiran)
+- 2019/05/13(月) 20:57:53 ["Create a New Repository"](https://github.com/new)
+```
+
+## License
+[MIT License](LICENSE)
+
+## Author
+[stakiran](https://github.com/stakiran)
---
+++ distory.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import os
+import sys
+import time
+
+def abort(msg):
+    print('Error!: {0}'.format(msg))
+    exit(1)
+
+def file2list(filepath):
+    ret = []
+    with open(filepath, encoding='utf8', mode='r') as f:
+        ret = [line.rstrip('\n') for line in f.readlines()]
+    return ret
+
+def list2file(filepath, ls):
+    with open(filepath, encoding='utf8', mode='w') as f:
+        f.writelines(['{:}\n'.format(line) for line in ls] )
+
+def file2str(filepath):
+    ret = ''
+    with open(filepath, encoding='utf8', mode='r') as f:
+        ret = f.read()
+    return ret
+
+def str2file(filepath, s):
+    with open(filepath, encoding='utf8', mode='w') as f:
+        f.write(s)
+
+
+def dtstr2dt(dtstr):
+    """ @param A YYMMDD string. """
+    return datetime.datetime(int('20'+dtstr[0:2]), int(dtstr[2:4]), int(dtstr[4:6]))
+
+def dt2unixtime_micro(dt):
+    unixtime_second = time.mktime(dt.timetuple())
+    unixtime_micro_str = '{}000000'.format(int(unixtime_second))
+    unixtime_micro = int(unixtime_micro_str)
+    return unixtime_micro
+
+def unixtime_micro_str2dt(unixtime_micro_str):
+    unixtime_second = int(unixtime_micro_str[:-6])
+    dt = datetime.datetime.fromtimestamp(unixtime_second)
+    return dt
+
+def walk_dt(dt, days):
+    """ @return A new dt object with walked +n days. """
+    return dt + datetime.timedelta(days=days)
+
+def dt2japanese_readable(dt):
+    weekdays = ['月','火','水','木','金','土','日']
+    idx = dt.weekday()
+    wdstr = weekdays[idx]
+
+    datestr = dt.strftime("%Y/%m/%d")
+    timestr = dt.strftime("%H:%M:%S")
+
+    ret = '{:}({:}) {:}'.format(datestr, wdstr, timestr)
+    return ret
+
+def dt2japanese_readable_without_time(dt):
+    dtstr = dt2japanese_readable(dt)
+    unneed_tail_length = len(' hh:mm:ss')
+    return dtstr[:-1*unneed_tail_length]
+
+def parse_arguments():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument('-p', '--profile-name', type=str, default=None, required=True,
+        help='Your firefox profile name. (Ex: XXXXXX.Username)')
+
+    parser.add_argument('-d', '--date', type=str, default=None, required=True,
+        help='A date you want to extract on. (Ex: 190214)')
+
+    parser.add_argument('--md', default=False, action='store_true',
+        help='If given, convert (DATE).csv to (DATE).md.')
+
+    args = parser.parse_args()
+    return args
+
+
+args = parse_arguments()
+
+profile_name = args.profile_name
+
+dtstr = args.date
+dt_start = dtstr2dt(dtstr)
+dt_end = walk_dt(dt_start, days=1)
+
+ut_start = dt2unixtime_micro(dt_start)
+ut_end = dt2unixtime_micro(dt_end)
+
+if not(args.md):
+    commandline_template = 'sqlite3 -readonly -csv "%appdata%\\Mozilla\\Firefox\\Profiles\\{profile_name}\\places.sqlite" "SELECT title,url,last_visit_date FROM moz_places WHERE last_visit_date BETWEEN {range_start} AND {range_end};" > {target_datestr}.csv'
+    commandline_params = {
+        'profile_name' : profile_name,
+        'range_start' : ut_start,
+        'range_end' : ut_end,
+        'target_datestr' : dtstr,
+    }
+    commandline = commandline_template.format(**commandline_params)
+
+    print(commandline)
+    returncode = os.system(commandline)
+    exit(returncode)
+
+target_filename = '{}.csv'.format(dtstr)
+output_filename = '{}.md'.format(dtstr)
+csv_lines = file2list(target_filename)
+
+md_lines = []
+for i,csv_line in enumerate(csv_lines):
+    historycount = i+1
+
+    # Normalization to avoid "comma in field"
+    csv_line = csv_line.replace(', ', ' / ')
+
+    title, url, unixtime_micro_str = csv_line.split(',', 2)
+    dt = unixtime_micro_str2dt(unixtime_micro_str)
+    dtstr_readable = dt2japanese_readable(dt)
+
+    # Completion title of specific urls
+    if len(title)==0:
+        title = 'Maybe redirect...'
+
+
+    md_line = '- {} [{}]({})'.format(
+        dtstr_readable, title, url
+    )
+
+    md_lines.append(md_line)
+caption = dt2japanese_readable_without_time(dtstr2dt(dtstr))
+md_lines.insert(0, '# {} {} counts'.format(caption, historycount))
+list2file(output_filename, md_lines)
